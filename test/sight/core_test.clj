bb7c655dcfc04e954cb051a9eaf4d11677961abf
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-04 21:19:12 +0530

    Write positive test case for null polling url scenario

--- test/sight/core_test.clj
+++ test/sight/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [sight.core :as core]
             [mock-clj.core :as m]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [sight.utils :as u]))
 
 (deftest unsupported-file-extension-test
   (testing "Should throw an exception when the given file-paths have an unsupported extension"
@@ -28,12 +29,50 @@
                    (.getMessage e)))
             (is (not (m/called? #'clj-http.client/post)))))))))
 
+(deftest sight-api-200-response-test
+  (testing "Should return the result when http call gives 200 status code without polling-url"
+    (let [client (core/->Client "12345678-1234-1234-1234-123456781234")]
+      (m/with-mock [clj-http.client/post    {:status 200
+                                             :body   (json/write-str {"RecognizedText" [{"Text"         "Invoice"
+                                                                                         "Confidence"   0.22863210084975458
+                                                                                         "TopLeftX"     395
+                                                                                         "TopLeftY"     35
+                                                                                         "TopRightX"    449
+                                                                                         "TopRightY"    35
+                                                                                         "BottomLeftX"  395
+                                                                                         "BottomLeftY"  47
+                                                                                         "BottomRightX" 449
+                                                                                         "BottomRightY" 47
+                                                                                         }]})}
+                    u/file-path->base64file "YWxzZGtmanNhbGtkZmogYWxzZGtmamFzbGtkZmphc2xka2ZqYXNkbGtmaiBhbHNrZGZqbHNhZA=="]
+        (let [result (core/recognize client
+                                     (list "/Users/johndoe/Downloads/baz.jpg"))]
+          (is (= {:pages [{:error                   ""
+                           :file-index              0
+                           :page-number             1
+                           :number-of-pages-in-file 1
+                           :recognized-text         [{:top-left-y     35,
+                                                      :bottom-right-y 47,
+                                                      :bottom-left-x  395,
+                                                      :top-right-x    449,
+                                                      :bottom-left-y  47,
+                                                      :top-right-y    35,
+                                                      :top-left-x     395,
+                                                      :bottom-right-x 449,
+                                                      :confidence     0.22863210084975458,
+                                                      :text           "Invoice"}]}]}
+                 result))
+          (is (= 1
+                 (m/call-count #'u/file-path->base64file)))
+          (is (= ["/Users/johndoe/Downloads/baz.jpg"]
+                 (m/last-call #'u/file-path->base64file))))))))
+
 (deftest sight-api-non-200-response-test
   (testing "Should throw an exception when the response status code is non 200"
     (let [client (core/->Client "12345678-1234-1234-1234-123456781234")]
       (m/with-mock [clj-http.client/post {:status 401
                                           :body   {:message "you are not authorized to use this api"}}
-                    core/make-payload "dummy-payload"]
+                    core/make-payload    "dummy-payload"]
         (try
           (core/recognize client
                           (list "/Users/johndoe/Downloads/baz.jpg" "/Users/johndoe/Downloads/bax.jpg"))
@@ -56,7 +95,7 @@
                    (m/last-call #'core/make-payload))))))
       (m/with-mock [clj-http.client/post {:status 500
                                           :body   {:message "Internal server error"}}
-                    core/make-payload "dummy-payload"]
+                    core/make-payload    "dummy-payload"]
         (try
           (core/recognize client
                           (list "/Users/johndoe/Downloads/baz.jpg" "/Users/johndoe/Downloads/bax.jpg"))
