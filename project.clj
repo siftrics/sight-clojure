63d6d0873c1accfde54849e95f5994ac0aae5979
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-30 11:16:46 +0530

    Add failjure

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [clj-http "3.10.0"]
                  [commons-codec/commons-codec "1.4"]
                  [mock-clj "0.2.1"]
-                 [camel-snake-kebab "0.4.1"]]
+                 [camel-snake-kebab "0.4.1"]
+                 [failjure "2.0.0"]]
   :cloverage {:fail-threshold 85}
   :plugins [[lein-cloverage "1.1.2"]]
   :deploy-repositories [["releases" :clojars]
